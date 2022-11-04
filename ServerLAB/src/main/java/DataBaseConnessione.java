b5c154a419f1e4212a585429d4af5fbb61e059a1
Gabriele1312 <gsassi2@studenti.uninsubria.it>
2022-11-04 12:13:21 +0100

    Prove connessione al databse + esecuzione query

--- ServerLAB/src/main/java/DataBaseConnessione.java
+++ ServerLAB/src/main/java/DataBaseConnessione.java
@@ -1,4 +1,90 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//implmenta pattern singleton 
 
 public class DataBaseConnessione {
+	
+	private final static String protocol = "jdbc:postgresql" + "://";
+	private static String host = "localhost/";
+	private static String resource = "LabB";
+	private static String url = protocol + host + resource;
+	private static String porta = "";
+	
+	private static String username = "postgres";
+	private static String password = "admin";
+	
+	private static Connection connection = null;
+	private static Statement statement = null;
+	
+	/**
+	 * Costruttore vuoto, dato che la classe implementa il pattern Singleton
+	 * */
+	
+	private DataBaseConnessione() {
+	}
+	
+	/**
+	 * Metodo che restituisce la Connection del database
+	 * 
+	 * @param user username con cui accedere al database
+	 * @param pass password con cui accedere al database
+	 * @param host2 indirizzo della macchina su cui &egrave; presente il database
+	 * @param port porta su cui gira il database
+	 * @param name nome del database
+	 * 
+	 * @throws SQLException se ci sono problemi nella connessione al database
+	 * 
+	 * @return un oggetto Connection, il quale si riferisce alla connessione appena istanziata
+	 * */
+	
+	public static Connection getConnection(String user, String pass, String host2, String port, String name) throws SQLException {
+		resource = name;
+		username = user;
+		password = pass;
+		porta = port;
+		host = host2 + "/";
+		url = protocol + host + resource;
+		if (connection == null) {
+			connection = DriverManager.getConnection(url, username, password);
+		}
+		return connection;
+	}
+	
+	/**
+	 * Metodo che chiude la connessione verso il database istanziato con getConnection
+	 * 
+	 * @throws SQLException se ci sono problemi di chiusura della connessione, come ad esempio la chiusura di una connessione non ancora aperta
+	 * */
+	public static void closeConnection() throws SQLException {
+		connection.close();
+		connection = null;
+	}
+	
+	/**
+	 * Metodo che restituisce un oggetto Statement dell'oggetto Connection istanziato con il metodo getConnection
+	 * 
+	 * @throws SQLException se ci sono problemi di connessione al database
+	 */		
+	public static Statement getStatement() throws SQLException {
+		if (statement == null) {
+			Connection connection = getConnection(username, password, host, porta, resource);
+			statement = connection.createStatement();
+		}
+		return statement; //lo statement è quello che esegue le query
+	}
+	
+	/**
+	 * Metodo che chiude la connessione verso il database istanziato con getConnection
+	 * 
+	 * @throws SQLException se ci sono problemi di chiusura della connessione, come ad esempio la chiusura di una connessione non ancora aperta
+	 * */
+	public static void closeStatement() throws SQLException {
+		((Connection) statement).close();
+		statement = null;
+	}
 
 }
+
--- ServerLAB/src/main/java/EsecutoreQuery.java
+++ ServerLAB/src/main/java/EsecutoreQuery.java
@@ -1,9 +1,28 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class EsecutoreQuery {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws SQLException {
 		
-
+		ResultSet rs; //risultato query organizzato in tabella
+		String query = "select * from Cittadini";
+		
+		Connection c = DataBaseConnessione.getConnection("postgres", "Sirmione2020!", "localhost", "5432", "LabB"); //prende connessione al database
+		Statement s = (Statement) c.createStatement(); //statement per eseguire query
+		
+	
+		rs = s.executeQuery(query);
+		
+		//se ci sono dei dati nel result set
+		while(rs.next()) {
+			System.out.print(rs.getString(1) + " "); //in questo caso stampa la stringa in posizione 0esima ecc..
+			System.out.print(rs.getString(2));
+			System.out.print(rs.getString(3));
+			System.out.println("");
+		}
 	}
 
 }
