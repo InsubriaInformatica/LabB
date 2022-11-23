d9637e3f07bb80c31292bae478c38181b13292e3
Batta <sbattaglia2@studenti.uninsubria.it>
2022-11-23 11:28:49 +0100

    jdoc

--- ServerLAB/src/main/java/ModelInterface.java
+++ ServerLAB/src/main/java/ModelInterface.java
@@ -2,11 +2,21 @@ import java.sql.SQLException;
 
 // PATTERN MVC --> interfaccia che fornisce elementi necessari all'implementazione
 public interface ModelInterface {
-	
-	public void updateModel(Object dati) throws SQLException; //aggiorna il model in base all'evento generato
-	
-	public void avvioServer(Object dati) throws SQLException; //contiene dati per avvio del server
-	
-	public void spegniServer(); //spegne server
+	/**
+	 * aggiorna il model in base all'evento generato
+	 * @param dati
+	 * @throws SQLException
+	 */
+	public void updateModel(Object dati) throws SQLException;
+	/**
+	 * contiene dati per avvio del server
+	 * @param dati
+	 * @throws SQLException
+	 */
+	public void avvioServer(Object dati) throws SQLException; 
+	/**
+	 * spegne server
+	 */
+	public void spegniServer(); 
 
 }
