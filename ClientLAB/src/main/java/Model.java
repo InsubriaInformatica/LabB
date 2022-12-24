de15797083a7ade36616a116e2356bda6ba263e2
aldoademi <aldo.ademi011299@gmail.com>
2022-12-24 13:36:52 +0100

    Javadoc Model

--- ClientLAB/src/main/java/Model.java
+++ ClientLAB/src/main/java/Model.java
@@ -1,13 +1,14 @@
-
 /**
  * PATTERN MVC --> questa interfaccia fornisce i prototipi necessari al funzionamento del model nell'applicazione
  * @author Gabriele Sassi
  *
  */
 public interface Model {
-	
-	//source --> evento di origine
-	//dati --> dati mandati dal controller e presi dalla view
-	public void updateModel(String source, Object dati); //metodo richiamato dal controller che aggiorna ed elabora dati da mostrare alla view
+	/**
+	 * Metodo richiamato dal controller che aggiorna ed elabora dati da mostrare alla view
+	 * @param source: evento di origine
+	 * @param dati: dati mandati dal controller e presi dalla view
+	 */
+	public void updateModel(String source, Object dati);
 
 }
